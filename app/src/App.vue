921cfddffb9cb1cd1822d40c069226040b14c3dd
Max Wardle <max.wardle@gov.bc.ca>
2021-05-13 14:33:01 -0700

    map renders

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Map msg="Welcome to Your Vue.js App"/>
+    <Map/>
   </div>
 </template>
 
--- app/src/components/Map.vue
+++ app/src/components/Map.vue
@@ -4,28 +4,20 @@
 
 <script>
 import maplibregl from 'maplibre-gl'; // or "const maplibregl = require('maplibre-gl');"
-console.dir(maplibregl)
 
 export default {
   name: 'Map',
   mounted () {
-    console.log('in init')
-
     this.createMap()
   },
   methods: {
     createMap: function () {
-
-      console.log('in create map')
-
       this.map = new maplibregl.Map({
         container: 'map',
-        style: 'https://api.maptiler.com/maps/outdoor/?key=Jit3oGuugSBYRmzQu0Qw#-0.2/0.00000/-2.36049', // #-0.2/0.00000/-2.36049
+        style: 'https://api.maptiler.com/maps/outdoor/style.json?key=Jit3oGuugSBYRmzQu0Qw',
         center: [-74.5, 40], // starting position [lng, lat]
         zoom: 9 // starting zoom
       });
-
-      console.dir(this.map)
     }
   }
 }
@@ -34,18 +26,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #map {position: absolute; top: 0; right: 0; bottom: 0; left: 0;}
 </style>
