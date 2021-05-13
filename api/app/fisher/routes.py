7c54d95cdf11d685abc82ec5f10ac3b9d5fbdcc7
Steve Hillier <stephenhillier@gmail.com>
2021-05-13 16:37:41 -0700

    use intersections

--- api/app/fisher/routes.py
+++ api/app/fisher/routes.py
@@ -44,18 +44,21 @@ def habitat_in_polygon(db: Session = Depends(get_db)):
     """ function goes here """
 
     q = """
-    with fisher_habitats as (
-        select  geom,
+    with cutblock as (
+        select ST_SetSRID(ST_GeomFromText(:cutblock), 3005) as geom
+    ),
+    fisher_habitats as (
+        select  ST_Transform(ST_Intersection(ST_Transform(geom, 3005), (select geom from cutblock)), 4326) as geom,
                 fisher_hab,
                 harvest_im,
                 denning_wa,
-                denning_pr * (shape_area / 10000) as denning_pr,
+                denning_pr * (ST_Area(ST_Intersection(ST_Transform(geom, 3005), (select geom from cutblock))) / 10000) as denning_pr,
                 branch_res,
-                branch_r_1 * (shape_area / 10000) as branch_r_1,
+                branch_r_1 * (ST_Area(ST_Intersection(ST_Transform(geom, 3005), (select geom from cutblock))) / 10000) as branch_r_1,
                 cavity_res,
-                cavity_r_1 * (shape_area / 10000) as cavity_r_1,
-                cwd_restin * (shape_area / 10000) as cwd_restin,
-                cwd_rest_1 * (shape_area / 10000) as cwd_rest_1
+                cavity_r_1 * (ST_Area(ST_Intersection(ST_Transform(geom, 3005), (select geom from cutblock))) / 10000) as cavity_r_1,
+                cwd_restin * (ST_Area(ST_Intersection(ST_Transform(geom, 3005), (select geom from cutblock))) / 10000) as cwd_restin,
+                cwd_rest_1 * (ST_Area(ST_Intersection(ST_Transform(geom, 3005), (select geom from cutblock))) / 10000) as cwd_rest_1
         from    fisher_fhe
         where   ST_Intersects(
                     geom,
@@ -67,17 +70,19 @@ def habitat_in_polygon(db: Session = Depends(get_db)):
     )
     select
         sum(denning_wa) as sum_denning_warning,
-        sum(denning_pr) as sum_denning_primary_density,
+        sum(denning_pr) as sum_denning_primary,
+        sum(denning_pr) / (select ST_Area(geom)/10000 from cutblock) as denning_primary_density,
         sum(branch_res) as sum_branch_resting_warning,
-        sum(branch_r_1) as sum_branch_resting_primary_density,
+        sum(branch_r_1) as sum_branch_resting_primary,
         sum(cavity_res) as sum_cavity_resting_warning,
-        sum(cavity_r_1) as sum_cavity_resting_primary_density,
-        sum(cwd_restin) as sum_resting_piece_density,
-        sum(cwd_rest_1) as sum_resting_piles_density,
+        sum(cavity_r_1) as sum_cavity_resting_primary,
+        sum(cwd_restin) as sum_resting_piece,
+        sum(cwd_rest_1) as sum_resting_piles,
         (select count(*) from fisher_habitats where harvest_im ilike 'warning%') as warning_count,
         (select count(*) from fisher_habitats where harvest_im ilike 'caution%') as caution_count,
         (select ST_AsGeoJSON(ST_CollectionExtract(ST_Collect(geom), 3)) from fisher_habitats where harvest_im ilike 'warning%') as warning_geom,
-        (select ST_AsGeoJSON(ST_CollectionExtract(ST_Collect(geom), 3)) from fisher_habitats where harvest_im ilike 'caution%') as caution_geom
+        (select ST_AsGeoJSON(ST_CollectionExtract(ST_Collect(geom), 3)) from fisher_habitats where harvest_im ilike 'caution%') as caution_geom,
+        (select sum(denning_pr) from fisher_habitats where harvest_im ilike 'warning%') as warning_denning_sum
     from fisher_habitats
     """
 
