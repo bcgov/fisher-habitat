c0b77b9df8b0594b986b67217d0a80b370bd6198
Max Wardle <max.wardle@gov.bc.ca>
2021-05-13 13:37:34 -0700

    initial map commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
-  name: 'Fisher Habitat',
+  name: 'FisherHabitat',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
---
+++ app/src/components/Map.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id='map'></div>
+</template>
+
+<script>
+import maplibregl from 'maplibre-gl'; // or "const maplibregl = require('maplibre-gl');"
+console.dir(maplibregl)
+
+export default {
+  name: 'Map',
+  mounted () {
+    console.log('in init')
+
+    this.createMap()
+  },
+  methods: {
+    createMap: function () {
+
+      console.log('in create map')
+
+      this.map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://api.maptiler.com/maps/outdoor/?key=Jit3oGuugSBYRmzQu0Qw#-0.2/0.00000/-2.36049', // #-0.2/0.00000/-2.36049
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      });
+
+      console.dir(this.map)
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
