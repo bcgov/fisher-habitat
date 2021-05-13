bedabebdf3cdd6a9a1c25a2695b8982947aecbcd
Steve Hillier <stephenhillier@gmail.com>
2021-05-13 13:36:01 -0700

    add start file

---
+++ api/start.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env sh
+# adapted from https://github.com/tiangolo/uvicorn-gunicorn-docker
+set -e
+
+export APP_MODULE=${APP_MODULE:-"main:app"}
+
+if [ -f /app/gunicorn_conf.py ]; then
+    DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
+elif [ -f /app/api/gunicorn_conf.py ]; then
+    DEFAULT_GUNICORN_CONF=/app/api/gunicorn_conf.py
+else
+    DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
+fi
+export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+
+# Let the DB start
+python /app/api/db/wait_for_db.py
+
+echo "$GUNICORN_CONF" "$APP_MODULE"
+
+# Start Gunicorn
+exec gunicorn -k uvicorn.workers.UvicornWorker -c "$GUNICORN_CONF" "$APP_MODULE"
