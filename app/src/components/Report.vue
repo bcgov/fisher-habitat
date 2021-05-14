3af274adc8add99e9bcf7531c74cfd0d7a36e46f
Luke Mitchell <lukegamitchell@gmail.com>
2021-05-14 07:28:00 -0700

    WIP report

--- app/src/components/Report.vue
+++ app/src/components/Report.vue
@@ -1,7 +1,51 @@
 <template>
-<div>
-  Report
-</div>
+  <div id='report'>
+        <h1>Fisher Habitat Retention Guidance</h1>
+        
+        <hr>
+
+        <div><b>Forest License: </b>A93054</div>
+        <div><b>Cutting Permit: </b>93054</div>
+        <div><b>Analysis Date: </b>2021-05-14</div>
+        <div><b>Retention Spatial Data Version: </b>210215</div>
+
+        <br>
+
+        <h2>Summary Table</h2>
+
+        <div><b>Proposed Cutblock Area (ha): </b>244.4</div>
+        <br>
+
+        <div><b>Harvest Impact Warning: </b>*</div>
+        <br>
+
+        <div><b>Denning Habitat</b></div>
+        <div><b>Acb >52 cm dbh or At >40 cm dbh: </b>1650</div>
+        <div><b>Contigency: next-largest diameter Acb or At: </b>6599</div> 
+        <br>
+
+        <div><b>Branch Resting Habitat</b></div>
+        <div><b>Sw >31 cm dbh or Sb >17 cm dbh with rust brooms: </b>55</div>
+        <div><b>Contigency: next-largest diameter Sw or any Sw with rust broom infection: </b>221</div>
+        <br>
+
+        <div><b>Cavity Resting Habitat</b></div>
+        <div>No retention needed for this habitat feature.</div>
+        <!-- TODO: What is the correct text for values here? -->
+        <!-- <div><b>Sw >31 cm dbh or Sb >17 cm dbh with rust brooms: </b>55</div>
+        <div><b>Contigency: next-largest diameter Sw or any Sw with rust broom infection: </b>221</div> -->
+        <br>
+
+        <div><b>Coarse Woody Debris Resting Habitat</b></div>
+        <div><b>Retention: </b><small>hard logs >35 cm diameter, >7 m in length, and elevated 25-50 cm above ground. If trees >35 cm do not occur in the harvest area (i.e., in cruise data), 2 or more smaller trees totalling >35 cm diameter may be placed alongside each other and elevated 25-50 cm above ground.</small> <b>342</b></div>
+        <div><b>Creation: </b><small>piles that are at least 3m x 5m and 2m high built with logs >10 cm diam. (no tops or fines), jumbled like pick-up sticks, with 1/3 of logs >20 cm diam. and >3 m long</small> <b>99</b></div>
+        <br>
+
+        <div><b>Warning Areas</b></div>
+        <div>???</div>
+        <br>
+
+  </div>
 </template>
 
 <script>
@@ -10,6 +54,9 @@ export default {
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#report {
+  text-align: left;
+}
 </style>
